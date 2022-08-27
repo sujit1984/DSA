99bc12447964bb602b63d9fb0d4040ff98211843
sujit1984 <sujitsugathan2002@gmail.com>
2022-08-27 22:47:44 +0530

    Adding the packages for the project

--- src/main/DataStructures/Arrays/Arrays.java
+++ src/main/DataStructures/Arrays/Arrays.java
@@ -7,6 +7,13 @@ public class Arrays {
         intArray[1] = 56;
         intArray[2] = 13;
         intArray[3] = 31;
+        intArray[4] = 51;
+        intArray[5] = 91;
+        intArray[6] = 89;
+
+        for (int j : intArray) {
+            System.out.println(j);
+        }
 
     }
 }
