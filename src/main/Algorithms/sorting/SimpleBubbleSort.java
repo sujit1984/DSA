8a543f12de05636f858309f547d1c5a22e26cb95
sujit1984 <sujitsugathan2002@gmail.com>
2022-08-29 22:49:01 +0530

    Added the time and space complexity comments

--- src/main/Algorithms/sorting/SimpleBubbleSort.java
+++ src/main/Algorithms/sorting/SimpleBubbleSort.java
@@ -13,13 +13,12 @@ public class SimpleBubbleSort {
     public static int[] BubbleSort(int[] arr){
             int n = arr.length;
             for (int i = 0 ; i < n; i++){
-                for(int j=1; j < (n-i); j++ ) {
+                for(int j=1; j < (n-i); j++ )
                     if (arr[j - 1] > arr[j]) {
                         int temp = arr[j - 1];
                         arr[j - 1] = arr[j];
                         arr[j] = temp;
                     }
-                }
             }
         return arr;
     }
