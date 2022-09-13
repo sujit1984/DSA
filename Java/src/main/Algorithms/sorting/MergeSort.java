a7d782e8570a4c49866f07d3e7e9540125eed55c
sujit1984 <sujitsugathan2002@gmail.com>
2022-09-13 22:30:12 +0530

    Added the code for the merge sort

---
+++ Java/src/main/Algorithms/sorting/MergeSort.java
@@ -0,0 +1,64 @@
+package main.Algorithms.sorting;
+
+import java.util.Scanner;
+
+public class MergeSort {
+    /*
+        # Author - Sujit S
+        # Date - 12 Sept 2022
+        # This is a basic implementation of the merge sert
+        # Time complexity = O(nlogn) , Space Complexity = O(1)
+     */
+
+    public static void mergeSort(int[] arr, int start , int end){
+        if (end - start < 2)    // this is the breaking condition for the recursive method
+            return;
+        // Now we are going to logically partition the array
+
+        int mid = (start + end)/2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid , end);
+        merge(arr , start, mid , end);
+    }
+
+    public static void merge( int[] arr, int start, int mid , int end){
+
+        if(arr[mid-1] <= arr [mid])
+            return;
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0 ;
+
+        int[] temp = new int[end - start];
+
+        while (i < mid && j < end){
+            temp[tempIndex++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
+
+        }
+
+        System.arraycopy(arr,i,arr,start+tempIndex, mid-i);
+
+        System.arraycopy(temp,0,arr, start, tempIndex);
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] intArray = new int[10];
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = sc.nextInt();
+        }
+        mergeSort(intArray,0, intArray.length);
+
+        Thread.sleep(3000);
+        System.out.println("Printing the sorted array.....");
+
+        for (int j : intArray) {
+            System.out.println(j);
+        }
+
+    }
+
+}
